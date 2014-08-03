492b590dba9dd6ac47d04ca9ac7068a5a24dc77b
Joshua Yanovski <pythonesque@gmail.com>
2014-08-03 12:51:09 -0700

    Got a letter to display

---
+++ src/gfx/word.rs
@@ -0,0 +1,65 @@
+use ft = freetype;
+use graphics::{
+    AddRectangle,
+    AddImage,
+    AddColor,
+    Context,
+    Draw,
+    ImageColorContext,
+    RectangleContext,
+    RelativeTransform2d,
+};
+use opengl_graphics::{
+    Gl,
+    Texture,
+};
+
+pub struct WordContext {
+    freetype: ft::Library,
+    face: ft::Face,
+    texture: Texture,
+}
+
+impl WordContext {
+    pub fn make() -> WordContext {
+        let freetype = ft::Library::init().unwrap();
+        static font: &'static str = "/usr/share/fonts/TTF/DejaVuSans.ttf";
+        let face = freetype.new_face(font, 0).unwrap();
+        face.set_pixel_sizes(0, 48).unwrap();
+        let ch = 'a';/*word.chars().next().unwrap();*/
+        face.load_char(ch as u64, ft::face::Render).unwrap();
+        let texture;
+        {
+            let g = face.glyph();
+            texture = Texture::from_memory_alpha(g.bitmap().buffer(), g.bitmap().width() as u32, g.bitmap().rows() as u32).unwrap();
+        }
+        WordContext { freetype: freetype, face: face, texture: texture }
+    }
+}
+
+pub struct WordBox<'dict, 'tree> {
+    ctx: ImageColorContext<'tree, Texture>,
+    //word: &'dict str,
+    //texture: Box<Texture>,
+}
+
+impl<'dict, 'tree> WordBox<'dict, 'tree> {
+    pub fn make<'dict, 'tree>(ctx: &Context, word_ctx: &'tree WordContext, word: &'dict str) -> WordBox<'dict, 'tree> {
+        let /*mut*/ x = 0;
+        let /*mut*/ y = 0;
+        /*for ch in word.chars() 
+        {*/
+            let g = word_ctx.face.glyph();
+            let newctx = (*ctx).trans((x + g.bitmap_left()) as f64, (y - g.bitmap_top()) as f64);
+            let newctx = newctx.image(&word_ctx.texture);
+            let newctx = newctx.rgb(0.0, 0.0, 0.0);
+            return WordBox { ctx: newctx };
+            /*x += (g.advance().x >> 6) as i32;
+            y += (g.advance().y >> 6) as i32;*/
+        /*}*/
+    }
+
+    pub fn draw(&self, back_end: &mut Gl) {
+        self.ctx.draw(back_end);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,67 @@
+extern crate graphics;
+extern crate freetype;
+extern crate piston;
+extern crate glfw_game_window;
+extern crate opengl_graphics;
+
 use gram::l0::{Noun, Verb, Pronoun, Determiner, S, ProNP, DetNomNP, Nominal, VP};
+use gfx::word::{WordBox, WordContext};
+
+use glfw_game_window::GameWindowGLFW;
+use opengl_graphics::Gl;
+
+use piston::{
+    Game,
+    GameWindow,
+    GameWindowSettings,
+    GameIteratorSettings,
+    RenderArgs,
+    UpdateArgs
+};
+
+use graphics::{
+    Context,
+    AddRectangle,
+    AddColor,
+    Draw,
+    RelativeTransform2d,
+};
 
 mod gram;
+mod gfx;
+
+pub struct App {
+    gl: Gl,       // OpenGL drawing backend.
+    rotation: f64, // Rotation for the square.
+    wc: WordContext,
+}
+
+impl <W: GameWindow> Game<W> for App {
+    fn render(&mut self, _: &mut W, args: &RenderArgs) {
+        // Set up a context to draw into.
+        let context = &Context::abs(args.width as f64, args.height as f64);
+        // Clear the screen.
+        context.rgba(0.0,1.0,0.0,1.0).draw(&mut self.gl);
+
+        // Draw a box rotating around the middle of the screen.
+        context
+            .trans((args.width / 2) as f64, (args.height / 2) as f64)
+            .rot_rad(self.rotation)
+            .rect(0.0, 0.0, 50.0, 50.0)
+            .rgba(1.0, 0.0, 0.0,1.0)
+            .trans(-25.0, -25.0)
+            .draw(&mut self.gl);
+
+//        let rect = context.rect(0.0, 1.0, 0.0, 1.0);
+        let ctx = WordBox::make(&context.trans(0.0, 100.0), &self.wc, "test");
+        ctx.draw(&mut self.gl);
+    }
+
+    fn update(&mut self, _: &mut W, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
+    }
+}
 
 fn main() {
     let nouns = [Noun("morning"), Noun("flight")];
@@ -14,4 +75,23 @@ fn main() {
     };
     println!("{:e}", s);
     println!("{}", s);
+    // Create a GLFW window.
+    let mut window = GameWindowGLFW::new(
+        GameWindowSettings {
+            title: "Hello Piston".to_string(),
+            size: [800, 800],
+            fullscreen: false,
+            exit_on_esc: true
+        }
+    );
+
+    // Some settings for how the game should be run.
+    let game_iter_settings = GameIteratorSettings {
+        updates_per_second: 60,
+        max_frames_per_second: 60
+    };
+
+    // Create a new game and run it.
+    let mut app = App { gl: Gl::new(), rotation: 0.0, wc: WordContext::make() };
+    app.run(&mut window, &game_iter_settings);
 }
